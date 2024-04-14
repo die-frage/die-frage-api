a9115b5f22d44329bc2add227cc1383bcd28862b
jekasrs <jekasjeny1012@gmail.com>
2024-04-14 22:03:24 +0300

    config refactored

---
+++ config-server/src/test/java/com/diefrage/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diefrage.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
