af6b4bbc6fb09d16116c86f89be2458ac9d665e9
jekasrs <jekasjeny1012@gmail.com>
2024-04-23 16:04:55 +0300

    init new server

---
+++ business-server/src/test/java/com/diefrage/businessserver/BusinessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diefrage.businessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
